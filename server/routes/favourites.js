0fab7a6669abce84ef29bb1b66790d3783b7b4ae
rapphcd <77208630+rapphcd@users.noreply.github.com>
2026-02-21 16:48:57 +0100

    add(sortable): ajout de positions aux favoris et possibilité de les réordonner.

--- server/routes/favourites.js
+++ server/routes/favourites.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const fileUpload = require("express-fileupload");
 const path = require("path");
 
-module.exports = function(app, dirpath){
+module.exports = function (app, dirpath) {
 
     app.post("/favourites/create", (req, res) => {
         let link = req.body.link;
@@ -10,73 +10,60 @@ module.exports = function(app, dirpath){
 
         fs.readFile('./favourites.json', 'utf8', (err, json) => {
             if (err) throw err;
-            let favourites = JSON.parse(json);
+            try {
+                let favourites = JSON.parse(json);
 
-            var id = 0;
-            if (favourites.length > 0) {
-                for(const fav of favourites){
-                    if(fav.id > id){
-                        id = fav.id
+                var id = 0;
+                var pos = 0;
+                if (favourites.length > 0) {
+                    for (const fav of favourites) {
+                        if (fav.id > id) {
+                            id = fav.id
+                        }
+                        if (fav.position > pos) {
+                            pos = fav.position
+                        }
                     }
                 }
-            }
-            id+=1
+                id += 1
+                pos += 1
 
-            if (!(link.startsWith("https://") || link.startsWith("http://"))) {
-                link = `https://${link}`
-            }
-
-            const newDatas = {
-                'id': id,
-                'link': link,
-                'name': name
-            }
-            favourites.push(newDatas)
-
-            let code = 200
-            fs.writeFile('./favourites.json', JSON.stringify(favourites), (err) => {
-                if (err) {
-                    code = 500
+                if (!(link.startsWith("https://") || link.startsWith("http://"))) {
+                    link = `https://${link}`
                 }
-            })
 
-            res.status(code).json(newDatas);
+                const newDatas = {
+                    'id': id,
+                    'link': link,
+                    'name': name,
+                    'position': pos
+                }
+                favourites.push(newDatas)
+
+                let code = 200
+                fs.writeFile('./favourites.json', JSON.stringify(favourites), (err) => {
+                    if (err) {
+                        code = 500
+                    }
+                })
+
+                res.status(code).json(newDatas);
+            } catch (e) {
+                return res.status(500);
+            }
         });
     });
 
-    app.put('/favourites/update/:id', (req, res) => {
+    app.put('/favourites/update/', (req, res) => {
         const updated = req.body.updated;
-        const id = req.params.id;
-
-        fs.readFile('./favourites.json', 'utf8', (err, json) => {
-            if (err) throw err;
-            let favourites = JSON.parse(json);
-
-            let newFavourites = [];
-            let found = false;
-
-            for (const favIndex in favourites) {
-                if (favourites[favIndex].id !== parseInt(id)) {
-                    newFavourites.push(favourites[favIndex])
-                } else {
-                    found = true
-                }
+        let code = 200
+        fs.writeFile('./favourites.json', JSON.stringify(updated), (err) => {
+            if (err) {
+                code = 500;
             }
+        })
+        res.status(code);
 
-            if (!found) {
-                res.status(404)
-            } else {
-                let code = 200;
-                newFavourites.push(updated);
-                fs.writeFile('./favourites.json', JSON.stringify(newFavourites), (err) => {
-                    if (err) {
-                        code = 500;
-                    }
-                })
-                res.status(code);
-            }
-
-        });
     });
 
     app.delete("/favourites/delete/:id", (req, res) => {
@@ -84,38 +71,46 @@ module.exports = function(app, dirpath){
 
         fs.readFile('./favourites.json', 'utf8', (err, json) => {
             if (err) throw err;
-            let favourites = JSON.parse(json);
+            try {
+                let favourites = JSON.parse(json);
 
-            let newDatas = []
-            for (const favIndex in favourites) {
-                const fav = favourites[favIndex];
-                if (fav.id !== parseInt(id)) {
-                    newDatas.push(fav)
+                let newDatas = []
+                for (const favIndex in favourites) {
+                    const fav = favourites[favIndex];
+                    if (fav.id !== parseInt(id)) {
+                        newDatas.push(fav)
+                    }
                 }
+
+                let code = 200
+                fs.writeFile('./favourites.json', JSON.stringify(newDatas), (err) => {
+                    if (err) {
+                        code = 500;
+                    }
+                })
+
+                res.status(code);
+            } catch (e) {
+                return res.status(500);
             }
-
-            let code = 200
-            fs.writeFile('./favourites.json', JSON.stringify(newDatas), (err) => {
-                if (err) {
-                    code = 500;
-                }
-            })
-
-            res.status(code);
         });
     });
 
     app.get("/favourites", (req, res) => {
         fs.readFile('./favourites.json', 'utf8', (err, json) => {
             if (err) throw err;
-            let favourites = JSON.parse(json);
-            res.json({favourites: favourites});
+            try {
+                let favourites = JSON.parse(json);
+                res.json({favourites: favourites});
+            } catch (e){
+                return res.status(500);
+            }
         });
     });
 
     app.get("/favourites/export", (req, res) => {
         res.download(dirpath + "/favourites.json", (err) => {
-            if(err) console.log(err);
+            if (err) console.log(err);
         })
     });
 
@@ -137,23 +132,27 @@ module.exports = function(app, dirpath){
 
         fs.readFile('./favourites.json', 'utf8', (err, json) => {
             if (err) return res.status(500);
-            const favourites = JSON.parse(json);
+            try {
+                const favourites = JSON.parse(json);
 
-            let toSend = "";
-            let found = false;
+                let toSend = "";
+                let found = false;
 
-            for (const favIndex in favourites) {
-                if (favourites[favIndex].id === parseInt(id)) {
-                    toSend = favourites[favIndex];
-                    found = true;
-                    break;
+                for (const favIndex in favourites) {
+                    if (favourites[favIndex].id === parseInt(id)) {
+                        toSend = favourites[favIndex];
+                        found = true;
+                        break;
+                    }
                 }
-            }
 
-            if (!found) {
-                res.status(404)
-            } else {
-                res.json({favourite: toSend});
+                if (!found) {
+                    res.status(404)
+                } else {
+                    res.json({favourite: toSend});
+                }
+            } catch (e) {
+                return res.status(500)
             }
         });
     });
